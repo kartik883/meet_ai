a9b0a4f5682054ff2a2fc5b141d2e473fe8ccdab
kartik883 <rajputkartik8448.com>
2026-02-04 12:04:41 +0530

    navbar for desktop and mobile

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { SidebarProvider } from '@/components/ui/sidebar';
+import { DashboardNavbar } from '@/modules/dashboard/ui/components/dashboard-navbar';
 import { DashboardSidebar } from '@/modules/dashboard/ui/components/dashboars-sidebar';
 import React from 'react'
 interface Props{
@@ -9,7 +10,8 @@ const Layout = ({children}:Props) => {
   return (
     <SidebarProvider>
       <DashboardSidebar/>
-        <main>
+        <main className='flex flex-col h-screen w-screen bg-muted'>
+          <DashboardNavbar/>
           {children}
         </main>
         
